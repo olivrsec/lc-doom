7d40e17ef5a38972015a2a300b7e261655b9f77e
olivrsec <127060948+olivrsec@users.noreply.github.com>
2026-01-31 01:39:55 +0000

    Update GeneratedPluginInfo.cs

--- src/src/GeneratedPluginInfo.cs
+++ src/src/GeneratedPluginInfo.cs
@@ -4,5 +4,5 @@ public static class GeneratedPluginInfo
 {
     public const string Identifier = "olivr.lethalcompany.doom";
     public const string Name = "LC-DOOM";
-    public const string Version = "1.2.0";
+    public const string Version = "2.0.0";
 }
